81377bb9eef3f46fb282a52d557dd1d9b12ccc6e
Nick Reich <nreich@pivotal.io>
2017-10-24 08:24:42 -0700

    GEODE-3801: Use hardlinks for backup oplog files (#963)
    * Oplog files that are backed up are read-only. For this reason,
      they can be transfered to the backup location through hard links,
      instead of copying the file. This change improves speed of backups.
      If the creation of a hard link fails, we revert to the existing copy
      behavior.
    * During backups, the copying of the oplog's krf file was being done while
      that file could still be in the process of writing. This change ensures
      that if a krf is to be written, that it is finished and included in the
      backup
    * cleanup existing oplog tests

--- geode-core/src/main/java/org/apache/geode/internal/cache/LocalRegion.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/LocalRegion.java
@@ -2049,6 +2049,7 @@ public class LocalRegion extends AbstractRegion implements InternalRegion, Loade
    *
    * @since GemFire 3.2
    */
+  @Override
   public DiskRegion getDiskRegion() {
     return this.diskRegion;
   }
